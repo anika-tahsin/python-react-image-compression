1636c256bf2d88c7f0410d110fad0908e2f8be1f
anika-tahsin <anikatahsintonu@gmail.com>
2024-04-22 16:43:01 +0600

    upload done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const ImageCompressionApp = () => {
   const [image, setImage] = useState(null);
   const [compressedImage, setCompressedImage] = useState(null);
   const [quality, setQuality] = useState(80); // Initial quality
+  const [file, setFile] = useState(null);
 
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
@@ -16,16 +17,28 @@ const ImageCompressionApp = () => {
     formData.append('image', image);
     formData.append('quality', quality);
 
-    axios.post('http://localhost:5000/compress', formData, {
-      responseType: 'blob'
-    })
-      .then(response => {
-        setCompressedImage(response.data);
-      })
-      .catch(error => {
-        console.error('Error compressing image:', error);
-      });
+    // axios.post('http://localhost:5000/compress', formData, {
+    //   responseType: 'blob'
+    // })
+    //   .then(response => {
+    //     setCompressedImage(response.data);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error compressing image:', error);
+    //   });
+    console.log("hello")
   };
+
+  const uploadHandler= async ()=>{
+    const formData = new FormData();
+    formData.append('photo', file); 
+    const res=await fetch("http://127.0.0.1:5000/api/upload",{
+      method:"POST",
+      body:formData
+    })
+    const result= await res.json();
+    console.log(result);
+  }
 
   const handleDownload = () => {
     const url = window.URL.createObjectURL(new Blob([compressedImage]));
@@ -39,7 +52,8 @@ const ImageCompressionApp = () => {
   return (
     <div> 
       <h1>Image Compression</h1>
-      <input type="file" onChange={handleImageChange} accept="image/*" />
+      {/* <input type="file" onChange={handleImageChange} accept="image/*" /> */}
+      <input type="file" accept="image/*"  onChange={(e) => setFile(e.target.files[0])} />
       {image && (
         <div>
           <h2>Preview:</h2>
@@ -57,7 +71,8 @@ const ImageCompressionApp = () => {
         />
         {quality}
       </div>
-      <button onClick={handleCompression}>Compress Image</button>
+      {/* <button onClick={handleCompression}>Compress Image</button> */}
+      <button onClick={uploadHandler}>Upload Image</button>
       {compressedImage && (
         <div>
           <h2>Compressed Image Preview:</h2>
